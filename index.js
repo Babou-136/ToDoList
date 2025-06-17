dd500160d281bac69203606e4116beb61b9fe2c6
Tristan Menard <tristan.menard@epitech.eu>
2025-06-17 18:48:14 +0200

    {add} button to display a task as done, and some css style (in order to display my website in darkmode)

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const form = document.getElementById("form");
 const text_task = document.getElementById("new_task");
-const todo = document.getElementById("ul")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
+    const li = document.createElement("li");
     const text = text_task.value.trim();
     if (text === "")
         return;
 
-    const task_added = document.createElement("li");
+    const task_added = document.createElement("span");
     task_added.textContent = text;
 
-    todo.appendChild(task_added);
+    const done = document.createElement("button");
+    done.textContent = "x";
+
+    done.addEventListener("click", () => {
+        task_added.style.textDecoration = "line-through";
+    })
+
+    li.appendChild(task_added);
+    li.appendChild(done);
     text_task.value = "";
+
+    document.getElementById("ul").appendChild(li);
 })
